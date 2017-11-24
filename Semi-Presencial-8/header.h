29299b74355764cb8a5359cd2517eeac5309830f
Gabriel Porto Oliveira <gabrielpoliveira@ig.com.br>
2017-11-24 20:53:37 -0200

    v0.0.1
    Projeto criado.

---
+++ Semi-Presencial-8/header.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   header.h
+ * Author: Asus
+ *
+ * Created on 24 de Novembro de 2017, 20:51
+ */
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HEADER_H */
+
---
+++ Semi-Presencial-8/main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Asus
+ *
+ * Created on 24 de Novembro de 2017, 20:51
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
